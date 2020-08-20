a0c6dccb2ec64aa66bf290bc769c18bcd93aea3b
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-08-20 21:01:56 +0300

    Added model for brands and middleware to handle user admin

--- server/server.js
+++ server/server.js
@@ -34,14 +34,43 @@ app.listen(port, () => {
 //                  MODELS
 //========================================
 const { User } = require('./models/user');
+const { Brand } = require('./models/brand');
 
 
 //========================================
 //              MIDDLEWARES
 //========================================
 const { auth } = require('./middleware/auth');
+const { admin } = require('./middleware/admin');
 
 
+//========================================
+//                  BRAND
+//========================================
+app.post('/api/product/brand', auth, admin, (req, res) => {
+    const brand = new Brand(req.body);
+
+    brand.save((err, doc) => {
+        if(err)
+            return res.json({
+                success: false,
+                err
+            });
+        res.status(200).json({
+            success: true,
+            brand: doc
+        })
+    })
+});
+
+app.get('/api/product/brand', (req,res) => {
+    Brand.find({}, (err, brands) => {
+        if(err)
+            return res.status(400).send(err);
+
+        return res.status(200).send(brands);
+    })
+})
 
 
 //========================================
