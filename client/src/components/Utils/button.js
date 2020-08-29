7c3107807323bc115ae9be979d4307a44cc44be7
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-08-29 20:26:27 +0300

    FrontEnd: new button type, card component update

--- client/src/components/Utils/button.js
+++ client/src/components/Utils/button.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag';
 
 
 const MyButton = (props) => {
@@ -7,9 +9,25 @@ const MyButton = (props) => {
         let template = '';
         switch(props.type){
             case "default":
-                template = <Link className = "link_default" to = {props.linkTo} {...props.addStyles}>
+                template = <Link 
+                className = {!props.altClass ? "link_default" : props.altClass}
+                to = {props.linkTo} 
+                {...props.addStyles}
+                >
                     {props.title}
                 </Link>
+                break;
+            case "bag_link":
+                template = 
+                    <div className="bag_link"
+                    onClick={() => {
+                        props.runAction();
+                    }}
+                    >
+                    <FontAwesomeIcon
+                            icon={faShoppingBag}
+                        />
+                    </div>
                 break;
             default : 
                 template = '';
