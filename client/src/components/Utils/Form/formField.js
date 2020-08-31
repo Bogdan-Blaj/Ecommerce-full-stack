3c5a866842ac60ce5f37ad7da7a7df00aa3237cc
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-08-31 20:16:36 +0300

    form add product, first input

--- client/src/components/Utils/Form/formField.js
+++ client/src/components/Utils/Form/formField.js
@@ -18,9 +18,15 @@ const FormField = ({ formData, change, id})  => {
     const renderTemplate = () => {
         let formTemplate = null;
         switch(formData.element){
+           
             case 'input':
                 formTemplate = (
                     <div className="formBlock">
+                    {
+                        formData.showlabel ?
+                        <div className="label_inputs">{formData.config.label}</div>
+                        : null
+                    }
                         <input {...formData.config}
                             value = {formData.value}
                             onBlur = {(event) => change({event, id, blur : true})}
