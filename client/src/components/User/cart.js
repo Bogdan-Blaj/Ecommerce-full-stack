b7a1334b7738e4107084973ac12f17a57af3d9b0
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-09-10 22:06:13 +0300

    cart component and total price added

--- client/src/components/User/cart.js
+++ client/src/components/User/cart.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import UserLayout from '../../hoc/userLayout';
 import { getCartItems } from '../../redux/actions/user_actions';
+import UserProductBlock from '../Utils/User/product_block';
 
 import { connect } from 'react-redux';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faFrown from '@fortawesome/fontawesome-free-solid/faFrown'
-import faSmile from '@fortawesome/fontawesome-free-solid/faSmile'
+import faFrown from '@fortawesome/fontawesome-free-solid/faFrown';
+import faSmile from '@fortawesome/fontawesome-free-solid/faSmile';
 
 class UserCart extends Component {
 
@@ -18,7 +19,7 @@ class UserCart extends Component {
     }
 
     componentDidMount(){
-        let cartItem = [];
+        let cartItems = [];
         let user = this.props.user;
 
         if(user.userData.cart){
@@ -26,22 +27,87 @@ class UserCart extends Component {
                 //get info from server
                 //push id to array
                 user.userData.cart.forEach(item => {
-                    cartItem.push(item.id);
+                    cartItems.push(item.id);
                 });
-                this.props.dispatch(getCartItems(cartItem, user.userData.cart))
-                .then(() => {
-                    
+                this.props.dispatch(getCartItems(cartItems, user.userData.cart))
+                    .then(() => {
+                        if(this.props.user.cartDetail.length > 0){
+                            this.calculateTotal(this.props.user.cartDetail);
+                        }
                 }) 
             }
         }
 
     }
 
+    calculateTotal = (cartDetail) => {
+        let total = 0;
+        cartDetail.forEach(item => {
+            total += parseInt(item.price, 10) * item.quantity;
+        });
+
+        this.setState({
+            total,
+            showTotal : true
+        })
+
+    }
+
+    removeFromCart = () => {
+
+    }
+
+    showNoItemMessage = () =>(
+        <div className="cart_no_items">
+            <FontAwesomeIcon icon={faFrown}/>
+            <div>
+                You have no items
+            </div>
+        </div>
+    )
+
     render() {
         return (
             <UserLayout>
                 <div>
-                    cart
+                    <h1>My cart</h1>
+                    <div className="user_cart">
+                        <UserProductBlock
+                            products={this.props.user}
+                            type="cart"
+                            removeItem={(id)=> this.removeFromCart(id)}
+                        />
+                        {
+                            this.state.showTotal ?
+                                <div>
+                                    <div className="user_cart_sum">
+                                        <div>
+                                            Total amount : $ {this.state.total}
+                                        </div>
+                                    </div>
+                                </div>
+                            : 
+                            this.state.showSuccess ?
+                            <div className="cart_success">
+                                <FontAwesomeIcon icon={faSmile}/>
+                                <div>
+                                    THANK YOU
+                                </div>
+                                <div>
+                                    YOUR ORDER IS NOW COMPLETE
+                                </div>
+                            </div>
+                        :
+                        this.showNoItemMessage()
+                    }
+                     </div>
+                     {
+                         this.state.showTotal ?
+                            <div className="paypal_button_container">
+                                Paypal
+                            </div>
+                        : null
+                     }
                 </div>
             </UserLayout>
         )
